10d232a9ad3fedf82177032efb9ba19e4c59054f
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-19 15:27:59 -0700

    add tag feature to questions

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -12,14 +12,17 @@ export default Ember.Component.extend({
     },
 
     newQuestionSave1() {
+      var tagArray = this.get('tags').split(',');
       var params ={
         content: this.get('content'),
         author: this.get('author'),
+        tags: tagArray
       };
       this.set('addNewQuestion', false);
       this.sendAction('sentFromNewQuestionComponent', params);
       this.set('content', "");
       this.set('author', "");
+      this.set('tags', "");
     }
   }
 });
